4ccceb65fa4734559f8216091feb08c7451b151e
Julian Schelker <julian.schelker@adnovum.ch>
2017-04-27 11:04:19 +0200

    formatting

--- src/app/contacts-detail-view/contacts-detail-view.component.ts
+++ src/app/contacts-detail-view/contacts-detail-view.component.ts
@@ -3,36 +3,31 @@ import { Router, ActivatedRoute } from '@angular/router';
 
 import { ContactsService } from '../contacts.service';
 import { Contact } from '../models/contact';
-//import { ContactsDetailComponent } from './contacts-detail/contacts-detail.component';
 
 @Component({
   selector: 'trm-contacts-detail-view',
   templateUrl: './contacts-detail-view.component.html',
   styleUrls: ['./contacts-detail-view.component.css']
 })
-export class ContactsDetailViewComponent implements OnInit  {
-   contact: Contact;
+export class ContactsDetailViewComponent implements OnInit {
+  contact: Contact;
 
   constructor(private router: Router,
     private contactsService: ContactsService,
     private route: ActivatedRoute) {
-
   }
 
 
- ngOnInit() {
-   this.contactsService.getContact(this.route.snapshot.paramMap.get('id'))
-     .subscribe(contact => this.contact = contact);
-
-}
+  ngOnInit() {
+    this.contactsService.getContact(this.route.snapshot.paramMap.get('id'))
+      .subscribe(contact => this.contact = contact);
+  }
 
   private navigateToEditor(contact: Contact) {
-   this.router.navigate(['./edit' ],{relativeTo:this.route});
-
-}
+    this.router.navigate(['./edit'], { relativeTo: this.route });
+  }
 
   private navigateToList() {
-   this.router.navigate(['/']);
-
-}
+    this.router.navigate(['/']);
+  }
 }
